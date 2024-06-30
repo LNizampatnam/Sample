2e1f4b0f2b980baffc718edbf6e4f0130b740d5f
Lakshmi Nizampatnam <151479712+LNizampatnam@users.noreply.github.com>
2024-06-29 19:04:34 -0500

    Add files via upload

--- http-server.js
+++ http-server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app     = express();
+const port = process.env.port || 3001;
 
 // serve static files from public directory
 // -------------------------------------------
